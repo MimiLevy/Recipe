55b45f51623bfd190f48406500d9ae4f7cae202d
Mimi Levy <264mimi@gmail.com>
2023-03-02 10:09:00 +0200

    Created recipe form

---
+++ RecipeApps/RecipeWinForms/frmRecipe.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace RecipeWinForms
+{
+    partial class lblCaptionStaffName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblMain = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCaptionCuisineType = new System.Windows.Forms.Label();
+            this.lblCaptionRecipeName = new System.Windows.Forms.Label();
+            this.lblCaptionCalories = new System.Windows.Forms.Label();
+            this.lblCaptionDateCreated = new System.Windows.Forms.Label();
+            this.lblCaptionDatePublished = new System.Windows.Forms.Label();
+            this.lblCaptionDateArchived = new System.Windows.Forms.Label();
+            this.lblCaptionRecipeStatus = new System.Windows.Forms.Label();
+            this.lblCaptionRecipePicture = new System.Windows.Forms.Label();
+            this.tblMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblMain
+            // 
+            this.tblMain.ColumnCount = 2;
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblMain.Controls.Add(this.label1, 0, 0);
+            this.tblMain.Controls.Add(this.lblCaptionCuisineType, 0, 1);
+            this.tblMain.Controls.Add(this.lblCaptionRecipeName, 0, 2);
+            this.tblMain.Controls.Add(this.lblCaptionCalories, 0, 3);
+            this.tblMain.Controls.Add(this.lblCaptionDateCreated, 0, 4);
+            this.tblMain.Controls.Add(this.lblCaptionDatePublished, 0, 5);
+            this.tblMain.Controls.Add(this.lblCaptionDateArchived, 0, 6);
+            this.tblMain.Controls.Add(this.lblCaptionRecipeStatus, 0, 7);
+            this.tblMain.Controls.Add(this.lblCaptionRecipePicture, 0, 8);
+            this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblMain.Location = new System.Drawing.Point(0, 0);
+            this.tblMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tblMain.Name = "tblMain";
+            this.tblMain.RowCount = 9;
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.46789F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.55046F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tblMain.Size = new System.Drawing.Size(433, 615);
+            this.tblMain.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Staff Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionCuisineType
+            // 
+            this.lblCaptionCuisineType.AutoSize = true;
+            this.lblCaptionCuisineType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionCuisineType.Location = new System.Drawing.Point(4, 68);
+            this.lblCaptionCuisineType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionCuisineType.Name = "lblCaptionCuisineType";
+            this.lblCaptionCuisineType.Size = new System.Drawing.Size(208, 68);
+            this.lblCaptionCuisineType.TabIndex = 1;
+            this.lblCaptionCuisineType.Text = "Cuisine Type";
+            this.lblCaptionCuisineType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionRecipeName
+            // 
+            this.lblCaptionRecipeName.AutoSize = true;
+            this.lblCaptionRecipeName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionRecipeName.Location = new System.Drawing.Point(4, 136);
+            this.lblCaptionRecipeName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionRecipeName.Name = "lblCaptionRecipeName";
+            this.lblCaptionRecipeName.Size = new System.Drawing.Size(208, 70);
+            this.lblCaptionRecipeName.TabIndex = 2;
+            this.lblCaptionRecipeName.Text = "Recipe Name";
+            this.lblCaptionRecipeName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionCalories
+            // 
+            this.lblCaptionCalories.AutoSize = true;
+            this.lblCaptionCalories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionCalories.Location = new System.Drawing.Point(4, 206);
+            this.lblCaptionCalories.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionCalories.Name = "lblCaptionCalories";
+            this.lblCaptionCalories.Size = new System.Drawing.Size(208, 65);
+            this.lblCaptionCalories.TabIndex = 3;
+            this.lblCaptionCalories.Text = "Calories";
+            this.lblCaptionCalories.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionDateCreated
+            // 
+            this.lblCaptionDateCreated.AutoSize = true;
+            this.lblCaptionDateCreated.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionDateCreated.Location = new System.Drawing.Point(4, 271);
+            this.lblCaptionDateCreated.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionDateCreated.Name = "lblCaptionDateCreated";
+            this.lblCaptionDateCreated.Size = new System.Drawing.Size(208, 68);
+            this.lblCaptionDateCreated.TabIndex = 4;
+            this.lblCaptionDateCreated.Text = "Date Created";
+            this.lblCaptionDateCreated.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionDatePublished
+            // 
+            this.lblCaptionDatePublished.AutoSize = true;
+            this.lblCaptionDatePublished.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionDatePublished.Location = new System.Drawing.Point(4, 339);
+            this.lblCaptionDatePublished.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionDatePublished.Name = "lblCaptionDatePublished";
+            this.lblCaptionDatePublished.Size = new System.Drawing.Size(208, 68);
+            this.lblCaptionDatePublished.TabIndex = 5;
+            this.lblCaptionDatePublished.Text = "Date Published";
+            this.lblCaptionDatePublished.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionDateArchived
+            // 
+            this.lblCaptionDateArchived.AutoSize = true;
+            this.lblCaptionDateArchived.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionDateArchived.Location = new System.Drawing.Point(4, 407);
+            this.lblCaptionDateArchived.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionDateArchived.Name = "lblCaptionDateArchived";
+            this.lblCaptionDateArchived.Size = new System.Drawing.Size(208, 68);
+            this.lblCaptionDateArchived.TabIndex = 6;
+            this.lblCaptionDateArchived.Text = "Date Archived";
+            this.lblCaptionDateArchived.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionRecipeStatus
+            // 
+            this.lblCaptionRecipeStatus.AutoSize = true;
+            this.lblCaptionRecipeStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionRecipeStatus.Location = new System.Drawing.Point(4, 475);
+            this.lblCaptionRecipeStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionRecipeStatus.Name = "lblCaptionRecipeStatus";
+            this.lblCaptionRecipeStatus.Size = new System.Drawing.Size(208, 68);
+            this.lblCaptionRecipeStatus.TabIndex = 7;
+            this.lblCaptionRecipeStatus.Text = "Recipe Status";
+            this.lblCaptionRecipeStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionRecipePicture
+            // 
+            this.lblCaptionRecipePicture.AutoSize = true;
+            this.lblCaptionRecipePicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaptionRecipePicture.Location = new System.Drawing.Point(4, 543);
+            this.lblCaptionRecipePicture.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCaptionRecipePicture.Name = "lblCaptionRecipePicture";
+            this.lblCaptionRecipePicture.Size = new System.Drawing.Size(208, 72);
+            this.lblCaptionRecipePicture.TabIndex = 8;
+            this.lblCaptionRecipePicture.Text = "Recipe Picture";
+            this.lblCaptionRecipePicture.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCaptionStaffName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 615);
+            this.Controls.Add(this.tblMain);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "lblCaptionStaffName";
+            this.Text = "frmRecipe";
+            this.tblMain.ResumeLayout(false);
+            this.tblMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblMain;
+        private Label label1;
+        private Label lblCaptionCuisineType;
+        private Label lblCaptionRecipeName;
+        private Label lblCaptionCalories;
+        private Label lblCaptionDateCreated;
+        private Label lblCaptionDatePublished;
+        private Label lblCaptionDateArchived;
+        private Label lblCaptionRecipeStatus;
+        private Label lblCaptionRecipePicture;
+    }
+}
