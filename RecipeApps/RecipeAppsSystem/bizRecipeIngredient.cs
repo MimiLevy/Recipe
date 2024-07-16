4408eb19a68fdf43aed74e3343f005379e453133
Mimi Levy <264mimi@gmail.com>
2024-07-16 11:25:31 +0300

    work not sure form what session

---
+++ RecipeApps/RecipeAppsSystem/bizRecipeIngredient.cs
@@ -0,0 +1,125 @@
+﻿using CPUFramework;
+using System.Data.SqlClient;
+
+namespace RecipeAppsSystem
+{
+    public class bizRecipeIngredient: bizObject<bizRecipeIngredient>
+    {
+        private int _recipeingredientid;
+        private int _recipeid;
+        private decimal _amount;
+        private int _ingredientid;
+        private int _measurementtypeid;
+        private string _ingredientname = "";
+        private string _measurementtypedesc = "";
+        private int _sequence;
+
+        public List<bizRecipeIngredient> LoadByRecipeId(int recipeid)
+        {
+            SqlCommand cmd = SQLUtility.GetSqlCommand("RecipeIngredientGet");
+            cmd.Parameters["@RecipeId"].Value = recipeid;
+            var dt = SQLUtility.GetDataTable(cmd);
+            return this.GetListFromDataTable(dt);
+        }
+
+        public int RecipeIngredientId
+        {
+            get { return _recipeingredientid; }
+            set
+            {
+                if(_recipeingredientid != value)
+                {
+                    _recipeingredientid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int RecipeId
+        {
+            get { return _recipeid; }
+            set
+            {
+                if (_recipeid != value)
+                {
+                    _recipeid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public decimal Amount
+        {
+            get { return _amount; }
+            set
+            {
+                if (_amount != value)
+                {
+                    _amount = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public int IngredientId
+        {
+            get { return _ingredientid; }
+            set
+            {
+                if (_ingredientid != value)
+                {
+                    _ingredientid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public int MeasurementTypeId
+        {
+            get { return _measurementtypeid; }
+            set
+            {
+                if (_measurementtypeid != value)
+                {
+                    _measurementtypeid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public string IngredientName
+        {
+            get { return _ingredientname; }
+            set
+            {
+                if (_ingredientname != value)
+                {
+                    _ingredientname = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public string MeasurementTypeDesc
+        {
+            get { return _measurementtypedesc; }
+            set
+            {
+                if (_measurementtypedesc != value)
+                {
+                    _measurementtypedesc = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public int Sequence
+        {
+            get { return _sequence; }
+            set
+            {
+                if (_sequence != value)
+                {
+                    _sequence = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ RecipeApps/RecipeAppsSystem/bizRecipeStep.cs
@@ -0,0 +1,69 @@
+﻿using CPUFramework;
+using System.Data.SqlClient;
+
+namespace RecipeAppsSystem
+{
+    public class bizRecipeStep: bizObject<bizRecipeStep>
+    {
+        private int _recipestepid;
+        private int _recipeid;
+        private string _stepdesc = "";
+        private int _sequence;
+        
+        public List<bizRecipeStep> LoadByRecipeId(int recipeid)
+        {
+            SqlCommand cmd = SQLUtility.GetSqlCommand("RecipeStepGet");
+            cmd.Parameters["@RecipeId"].Value = recipeid;
+            var dt = SQLUtility.GetDataTable(cmd);
+            return this.GetListFromDataTable(dt);
+        }
+        public int RecipeSstepId
+        {
+            get { return _recipestepid; }
+            set
+            {
+                if (_recipestepid != value)
+                {
+                    _recipestepid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public int RecipeId
+        {
+            get { return _recipeid; }
+            set
+            {
+                if (_recipeid != value)
+                {
+                    _recipeid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public string StepDesc
+        {
+            get { return _stepdesc; }
+            set
+            {
+                if (_stepdesc != value)
+                {
+                    _stepdesc = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+        public int Sequence
+        {
+            get { return _sequence; }
+            set
+            {
+                if (_sequence != value)
+                {
+                    _sequence = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+    }
+}
